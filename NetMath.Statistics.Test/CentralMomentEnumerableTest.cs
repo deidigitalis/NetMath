fa682f448e9253708be49468dad70988510c7757
Pablo Caballero <deidigitalis@gmail.com>
2016-05-13 16:28:34 +0200

    Calculation of Central Moment (m-r) and CentralMomentAboutMean (μ-r).

---
+++ NetMath.Statistics.Test/CentralMomentEnumerableTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetMath.Statistics.Moment;
+using NetMath.Statistics.Variance;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetMath.Statistics.Test
+{
+    [TestClass]
+    public class CentralMomentEnumerableTest
+    {
+        [TestMethod]
+        public void TestCentralMomentAboutMeanR1Equals0()
+        {
+            var distribution = CreateDistribution();
+            double μ1 = distribution.CentralMomentAboutMean(1);
+            Assert.AreEqual(0, μ1, 1e-12);
+        }
+
+        [TestMethod]
+        public void TestCentralMomentAboutMeanR2EqualsVariance()
+        {
+            var distribution = CreateDistribution();
+            double variance = distribution.Variance();
+            double μ2 = distribution.CentralMomentAboutMean(2);
+            Assert.AreEqual(variance, μ2, 1e-12);
+        }
+
+        [TestMethod]
+        public void TestCentralMomentR1EqualsArithmeticMean()
+        {
+            var distribution = CreateDistribution();
+            double arithmeticMean = distribution.Average();
+            double μ1 = distribution.CentralMoment(1);
+            Assert.AreEqual(arithmeticMean, μ1);
+        }
+
+        [TestMethod]
+        public void TestCentralMomentR2PropertyVariance()
+        {
+            var distribution = CreateDistribution();
+            double variance = distribution.Variance();
+            double m1 = distribution.CentralMoment(1);
+            double m2 = distribution.CentralMoment(2);
+            Assert.AreEqual(variance, m2 - m1 * m1);
+        }
+
+        private static IList<double> CreateDistribution()
+        {
+            return new List<double> { 0, 2, 2, 3, 5, 6, 6, 8, 9 };
+        }
+    }
+}
---
+++ NetMath.Statistics/Moment/CentralMomentEnumerable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetMath.Statistics.Moment
+{
+    public static class CentralMomentEnumerable
+    {
+        /// <summary>
+        /// Central moment about the origin
+        /// </summary>
+        /// <param name="distribution">Sorted distribution</param>
+        /// <param name="r">r-moment</param>
+        /// <returns>m(r)</returns>
+        public static double CentralMoment(this IList<double> distribution, int r)
+        {
+            if (distribution == null) throw new ArgumentNullException(nameof(distribution));
+
+            if (r <= 0) throw new ArgumentOutOfRangeException(nameof(r));
+
+            var table = distribution.ConvertToValueFrecuencyPair();
+            return table.Sum(x => x.Frecuency * Math.Pow(x.Value, r)) / table.Sum(x => x.Frecuency);
+        }
+
+        /// <summary>
+        /// Central moment about the airthmetic mean
+        /// </summary>
+        /// <param name="distribution">Sorted distribution</param>
+        /// <param name="r">r-moment</param>
+        /// <returns>μ(r)</returns>
+        public static double CentralMomentAboutMean(this IList<double> distribution, int r)
+        {
+            if (distribution == null) throw new ArgumentNullException(nameof(distribution));
+
+            if (r <= 0) throw new ArgumentOutOfRangeException(nameof(r));
+
+            double avg = distribution.Average();
+
+            var table = distribution.ConvertToValueFrecuencyPair();
+
+            return table.Sum(x => x.Frecuency * Math.Pow(x.Value - avg, r)) / table.Sum(x => x.Frecuency);
+        }
+    }
+}
